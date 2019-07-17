a7225bf37bccbabb19e609c23708fe9b6d46e675
Sellec <echelonww@gmail.com>
2019-07-17 16:57:40 +0300

    1

--- Projects/Standard/Plugins/FileManager/CustomFieldsFileTypes/FileImageFieldTypeRender.cs
+++ Projects/Standard/Plugins/FileManager/CustomFieldsFileTypes/FileImageFieldTypeRender.cs
@@ -118,7 +118,7 @@ namespace OnWeb.Plugins.FileManager.CustomFieldsFileTypes
                 }
             }
 
-            str += "</div>";
+            str += $"</div><input type='hidden' name='fieldValue_{field.IdField}[]'>";
 
             return MvcHtmlString.Create(str);
         }
