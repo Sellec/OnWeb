692b3668f7d7ddeb2996bf08ac557d51e0f65639
Sellec <echelonww@gmail.com>
2019-07-29 18:49:59 +0300

    1

--- Projects/CoreBind/NetFramework/CoreBind/Modules/ModuleActionInvoker.cs
+++ Projects/CoreBind/NetFramework/CoreBind/Modules/ModuleActionInvoker.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Reflection;
 using System.Web.Mvc;
+using System.Linq;
 using System.Web.Mvc.Async;
 
 namespace OnWeb.CoreBind.Modules
@@ -24,10 +25,10 @@ namespace OnWeb.CoreBind.Modules
 
         protected override ActionDescriptor FindAction(ControllerContext controllerContext, ControllerDescriptor controllerDescriptor, string actionName)
         {
-            var ControllerType = controllerDescriptor.ControllerType;
-            MethodInfo[] methods = ControllerType.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.InvokeMethod);
-            var ActionMethods = Array.FindAll<MethodInfo>(methods, new Predicate<MethodInfo>(this.IsValidActionMethod));
-            var StandardRouteMethods = new HashSet<MethodInfo>(ActionMethods);
+            var controllerType = controllerDescriptor.ControllerType;
+            var methods = controllerType.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.InvokeMethod);
+            var actionMethods = Array.FindAll<MethodInfo>(methods, new Predicate<MethodInfo>(IsValidActionMethod));
+            var standardRouteMethods = new HashSet<MethodInfo>(actionMethods);
 
             if (typeof(Internal.IModuleControllerInternalErrors).IsAssignableFrom(controllerDescriptor.ControllerType))
             {
@@ -38,7 +39,12 @@ namespace OnWeb.CoreBind.Modules
 
             if (action == null)
             {
-
+                var actionMethodCandidate = actionMethods.FirstOrDefault(x => x.Name == actionName);
+                if (actionMethodCandidate != null)
+                {
+                    var parametersValues = actionMethodCandidate.GetParameters().ToDictionary(x => x.Name, x => new CustomReflectedActionParameter() { HasDefaultValue = x.HasDefaultValue, Value = x.HasDefaultValue ? x.DefaultValue : null });
+                    action = new ReflectedActionDescriptor(actionMethodCandidate, actionName, controllerDescriptor);
+                }
             }
 
             return action;
@@ -61,10 +67,10 @@ namespace OnWeb.CoreBind.Modules
 
         protected override ActionDescriptor FindAction(ControllerContext controllerContext, ControllerDescriptor controllerDescriptor, string actionName)
         {
-            var ControllerType = controllerDescriptor.ControllerType;
-            MethodInfo[] methods = ControllerType.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.InvokeMethod);
-            var ActionMethods = Array.FindAll<MethodInfo>(methods, new Predicate<MethodInfo>(this.IsValidActionMethod));
-            var StandardRouteMethods = new HashSet<MethodInfo>(ActionMethods);
+            var controllerType = controllerDescriptor.ControllerType;
+            var methods = controllerType.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.InvokeMethod);
+            var actionMethods = Array.FindAll<MethodInfo>(methods, new Predicate<MethodInfo>(IsValidActionMethod));
+            var standardRouteMethods = new HashSet<MethodInfo>(actionMethods);
 
             if (typeof(Internal.IModuleControllerInternalErrors).IsAssignableFrom(controllerDescriptor.ControllerType))
             {
@@ -75,7 +81,12 @@ namespace OnWeb.CoreBind.Modules
 
             if (action == null)
             {
-
+                var actionMethodCandidate = actionMethods.FirstOrDefault(x => x.Name == actionName);
+                if (actionMethodCandidate != null)
+                {
+                    var parametersValues = actionMethodCandidate.GetParameters().ToDictionary(x => x.Name, x => new CustomReflectedActionParameter() { HasDefaultValue = x.HasDefaultValue, Value = x.HasDefaultValue ? x.DefaultValue : null });
+                    action = new ReflectedActionDescriptor(actionMethodCandidate, actionName, controllerDescriptor);
+                }
             }
 
             return action;
