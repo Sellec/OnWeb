22e9bedc039fc032cbda1f369cf3d53d8d8bd7a9
Sellec <echelonww@gmail.com>
2019-08-20 05:49:54 +0300

    1

--- Projects/FakeForPhoneNumbers/Properties/AssemblyInfo.cs
+++ Projects/FakeForPhoneNumbers/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("CoreFakeForLibphonenumbers")]
+[assembly: AssemblyTitle("FakeForPhoneNumbers")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CoreFakeForLibphonenumbers")]
+[assembly: AssemblyProduct("FakeForPhoneNumbers")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
